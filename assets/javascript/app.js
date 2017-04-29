0211d05d0ee00c932f0edf103ecdf793861e8b34
Maria <msawaby@gmail.com>
2017-04-29 09:39:57 -0700

    documented

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,24 @@
-
+// predefined array of animals for buttons
 var buttonArray = ["Rabit", "Monkey", "Horse", "Lion", "Wolf", "Tiger", "Bird"];
-
+// whenever this function is called it will take the input from the user and
+//will create the button and will add it to the DOM 
 function addButton(){
+	// make button-panel and input empty first in order to avoid duplicates
 	$("#button-panel").empty();
 	$("#input-btn").val('');
+	//this will go through array and create buttons for each item in the array
 	for(var i=0; i<buttonArray.length; i++){
 	var button = $("<button>");
 	button.addClass("btn btn-default giphy-btn");
-	button.attr("id", "item-"+i);
+	//button.attr("id", "item-"+i);
 	button.attr("name", buttonArray[i]);
+	//style for the button
 	button.css({"background-color": "#006666", "color": "#fff", "margin":"5px"});
 	button.text(buttonArray[i]);
-	$("#button-panel").append(button);
+	$("#button-panel").append(button); //add to button panel
 	}
 }
+//calling function to show buttons when the page loads
 addButton();
 
 
@@ -25,63 +30,59 @@ $("#add-button").on("click", function(){
 		addButton();
 	}
 });
+
 var animate=[];
 var static= [];
 var staticIMG= [];
 var animateIMG = [];
 // display images
-function display(name){
-
-	
+function display(name){	
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q="+name+"&api_key=dc6zaTOxFJmzC&limit=10";
+	//calling ajax to send a query to the API and GET the response
 	$.ajax({
 		url: queryURL,
 		method: "GET"
-	}).done(function(response){
+	}).done(function(response){    //when data is fetched from the API do the following
 		$('#image-display').empty();
 		console.log(response);
+		//go through response and display each image
 		for(var i = 0; i< response.data.length; i++){
 			var span = $("<span>");
 			span.css({"float":"left", "margin": "10px"});
-			// span.attr('id', 'item-'+i)
 			var p = $("<p>");
 			p.text("Rating: "+response.data[i].rating);
 			span.append(p);
 			var img = $("<img>");
 			var status = img.attr("data-state", "still");
 			img.addClass("img");
-			img.attr("name", name)
+			img.attr("name", name);
+			//saving each url in an array of animate and static for using in animating or stoping image
 			static[i] = response.data[i].images.fixed_height_still.url;
 			animate[i] = response.data[i].images.fixed_height.url;
-			// staticIMG[i] = img.attr('id', 'item-'+i);
-			// console.log(staticIMG[i]);
-			// animateIMG[i] = img.attr('id', 'item-'+i);
 			img.attr("src", static[i]);
 			span.append(img);
 			$("#image-display").append(span);
-			
-			
 		}
 	});
 }
+//when the user clicks on any of the button of animals it will take it and send the data to display()
 $(document).on("click", ".giphy-btn", function(){
 	var name = $(this).attr("name");
 	console.log(name);
 	display(name);
 });
 
-
+//when any image on the screen is clicked it will change from still to animate and vise versa
 $(document).on("click", "img",function(){
 	
 	var name = $(this).attr("name");
 	console.log(name);
 	dataState = $(this).attr("data-state");
 	console.log(dataState);
-	// var idIMG = $(this).attr('id');
-	
+	//find the index of span to replace its static image with animated	
 	var index1 = $('span').index($(this).parent());
 	console.log("index is "+index1);
-	
+	//if state of img is still change it to animate, else change it to still
 	if(dataState === "still"){
 		console.log(static);
 		$(this).attr("data-state", "animate");
